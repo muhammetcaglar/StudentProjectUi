a664774a1c97799d53bbda246a471cdeedd765f3
Your New Name <Your new email>
2023-11-08 22:25:36 +0300

    first commit

---
+++ src/app/students/students.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from './student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit{
+
+
+  constructor(private studentService:StudentService) {
+
+  }
+  ngOnInit(): void {
+    debugger;
+    this.studentService.getStudents().subscribe(
+
+      (success)=>{
+
+      },
+      (err)=>{
+
+      }
+    )
+  }
+
+}
